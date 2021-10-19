447faf12cea4d4a60ec9dfcdd380618d1e57922b
BillyKuiper <456181@student.fontys.nl>
2021-10-19 20:59:53 +0200

    extra configuration

---
+++ src/views/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <form @submit.prevent="submitLogin">
+    <h1 class="h3 mb-3 fw-normal">Please Login</h1>
+    <input v-model="email" type="email" class="form-control" placeholder="Email" required>
+    <input v-model="password" type="password" class="form-control" placeholder="Password" required>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+  </form>
+</template>
+
+<script lang="js">
+
+import axios from "axios";
+
+export default {
+  name: "Login",
+  data(){
+    return{
+      email: "",
+      password: ""
+    }
+  },
+  methods:{
+    submitLogin(){
+      const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem("token")
+      }
+
+      axios.get('https://localhost:44368/Authentication/login',{
+        "email": this.email,
+        "password": this.password
+      },{headers: headers})
+          .then((response) => {
+            console.log(response)
+            //this.$router.push("/login")
+          })
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
